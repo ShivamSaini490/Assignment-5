43653689355adc58580c6df8bbae5834914bb2b1
shivam <shivam@DESKTOP-BT1O840>
2023-08-25 22:19:05 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+partial class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+partial class Person
+{
+    public void PrintFullName()
+    {
+        Console.WriteLine($"Full Name: {FirstName} {LastName}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.FirstName = "Shivam";
+        person.LastName = "Saini";
+
+        person.PrintFullName();
+    }
+}
